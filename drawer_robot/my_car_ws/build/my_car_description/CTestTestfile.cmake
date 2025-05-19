e6964829193aa7a52825c7d94633e49fcd295361
La-guajolota <adriansilpa@gmail.com>
2025-05-18 19:58:24 -0600

    my_car_description pkg

---
+++ drawer_robot/my_car_ws/build/my_car_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/adrian/sd_linux/sem8/Robotica/drawer_robot/my_car_ws/src/my_car_description
+# Build directory: /media/adrian/sd_linux/sem8/Robotica/drawer_robot/my_car_ws/build/my_car_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

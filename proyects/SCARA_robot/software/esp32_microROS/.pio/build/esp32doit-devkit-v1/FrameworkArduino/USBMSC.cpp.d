a81bab28e00bd626e36609f6fa54adb0b2db16ab
La-guajolota <adriansilpa@gmail.com>
2025-06-13 02:13:57 -0600

    version1 scararobot

---
+++ proyects/SCARA_robot/software/esp32_microROS/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
+ /home/adrian/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /home/adrian/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/adrian/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
